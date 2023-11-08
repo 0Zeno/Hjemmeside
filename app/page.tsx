ae704902782b93455f932789b1b55f1c41cc8c4d
Zeno Leonardi <zenoelioleone@gmail.com>
2023-11-08 15:51:34 +0100

    Endret litt på hjemmesiden

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Socials } from "@/components/socials";
 export default function Home() {
   return (
     <div className="max-w-2xl mx-auto">
-      <h1 className="text-3xl text-[#ff00ff] text-center">Zeno Elio Leonardi</h1>
+      <h1 className="text-3xl text-center">Zeno Elio Leonardi</h1>
       <Socials />
     </div>
   );
--- components/socials.tsx
+++ components/socials.tsx
@@ -9,24 +9,27 @@ function SocialLink({
   children: React.ReactNode;
 }) {
   return (
-    <Link
-      className="hover:underline text-blue-600 flex flex-col justify-between"
-      href={href}
-    >
-      {children}
-    </Link>
+    <div className="">
+      <Link className="hover:underline text-blue-600 flex" href={href}>
+        {children}
+      </Link>
+    </div>
   );
 }
 
 export function Socials() {
   return (
-    <div className="flex justify-between">
-      <h1>Sosiale medier</h1>
+    <div className="flex flex-col">
       {links.map((link) => {
         return (
-          <SocialLink key={link.name} href={link.href}>
-            {link.name} {link.icon}
-          </SocialLink>
+          <div className="flex flex-row justify-between">
+            <SocialLink key={link.name} href={link.href}>
+              {link.name} 
+            </SocialLink>
+            <div className="">
+            {link.icon}
+            </div>
+          </div>
         );
       })}
     </div>
