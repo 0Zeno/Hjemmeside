441ad1ffb6e01209a61a816d8eac6630f23e72a3
Zeno Leonardi <zenoelioleone@gmail.com>
2023-11-13 13:59:13 +0100

    La til key i about

--- components/about.tsx
+++ components/about.tsx
@@ -11,7 +11,7 @@ export function AboutMe() {
 return (
   <div>
     {texts.map((text) => (
-      <p className="pt-8 pb-2 border-gray-400 border-b">{text.info}</p>
+      <p className="pt-8 pb-2 border-gray-400 border-b" key={text.info}>{text.info}</p>
     ))}
   </div>
 )
