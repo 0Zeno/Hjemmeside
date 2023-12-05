57790befc190fe5a9db618028683e5fdfc2dc09f
Zeno Leonardi <zenoelioleone@gmail.com>
2023-12-05 02:16:36 +0100

    Spelling mistake

--- components/projects.tsx
+++ components/projects.tsx
@@ -2,7 +2,7 @@ import { Link } from "lucide-react";
 
 const projectslist = [
   {
-    projectname: "echo - Websuite",
+    projectname: "echo - Webkom",
     discription:
       "Webkom is a subgroup of echo that operates and further develops echo's web solutions.",
     link: "echo.uib.no",
@@ -17,9 +17,9 @@ export default function Projects() {
         return (
           <div key={project.projectname} className="pb-2 flex">
             <div className="border border-border p-2 space-y-2">
-              <h2 className="p-2 font-medium">{project.projectname}</h2>
+              <a href="" className="p-2 font-medium">{project.projectname}</a>
               <p className="p-2 w-64">{project.discription}</p>
-              <a className="p-2" href="https://echo.uib.no/" target="_blank">{project.link}</a>
+              <a className="p-2 hover:underline" href="https://echo.uib.no/" target="_blank">{project.link}</a>
               <p className="border border-border p-2">{project.tech}</p>
             </div>
           </div>
