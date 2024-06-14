7e60c18e5e6a2ebcf597852f4c1168ad4e21ce83
Zeno Elio Leonardi <zenoelioleone@gmail.com>
2024-06-14 12:26:20 +0200

    La til skole prosjekter og gjorde den reactive

--- components/prosjekter.tsx
+++ components/prosjekter.tsx
@@ -1,6 +1,6 @@
 import { Link } from "lucide-react";
 
-const projectslist = [
+const groupProjects = [
   {
     projectname: "echo Webkom",
     discription:
@@ -12,29 +12,81 @@ const projectslist = [
   {
     projectname: "echo Consulting",
     discription:
-      "echo Consulting er en 'startup' som utvikler og drifer webløsninger for linjeforedninger.",
-      linknavn: "echo-consulting.no",
+      "echo Consulting er en 'startup' som utvikler og drifer webløsninger for linjeforedninger og bedrifter.",
+    linknavn: "echo-consulting.no",
     link: "https://echo-consulting.no/",
     tech: "Next.js",
+  },
+];
+
+const schoolProjects = [
+  {
+    projectname: "Tetris",
+    discription:
+      "Lagde min versjon av tetris i java. Spillet lagde jeg ved å følge en guide som faglærer lagde til oss.",
+    linknavn: "Lenke til github",
+    link: "https://github.com/0Zeno/Tetris-INF101",
+    tech: "Java",
+    fag: "INF101",
+    år: "vår 2024",
+  },
+  {
+    projectname: "Endless Hopper",
+    discription:
+      "Endless hopper er et spill jeg lagde i java helt fra scratch. Spillet går ut på at man enten kan hoppe til høyre eller venstre på en platform som faller fortere og fortere",
+    linknavn: "Lenke til github",
+    link: "https://github.com/0Zeno/EndlessHopper",
+    tech: "Java",
+    fag: "INF101",
+    år: "vår 2024",
   },
 ];
 
 export default function Projects() {
   return (
     <div className="flex">
-      <div className="flex md:flex-row md:space-x-2 space-y-2 md:space-y-0 flex-col">
+      <div className="space-y-4">
+        <h1 className="flex text-3xl text-shadow font-semibold">
+          Gruppe prosjekter
+        </h1>
+        <div className="flex sm:flex-row flex-col sm:space-x-4 space-y-4 sm:space-y-0">
+          {groupProjects.map((project) => {
+            return (
+              <div key={project.projectname}>
+                <div className="border border-border rounded-md p-2 w-64">
+                  <p className="font-bold p-2 text-shadow">
+                    {project.projectname}
+                  </p>
+                  <p className="p-2 w-64">{project.discription}</p>
+                  <a href={project.link} className="p-2 hover:text-shadow">
+                    {project.linknavn}
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <h1 className="flex text-3xl text-shadow font-semibold">
+          Skole prosjekter
+        </h1>
 
-      {projectslist.map((project) => {
-        return (
-          <div key={project.projectname}>
-            <div className="border border-border p-2">
-              <p className="font-bold p-2 text-shadow">{project.projectname}</p>
-              <p className="p-2 w-64">{project.discription}</p>
-              <a href={project.link} className="p-2 hover:text-shadow">{project.linknavn}</a>
-            </div>
-          </div>
-        );
-      })}
+        <div className="flex sm:flex-row flex-col sm:space-x-4 space-y-4 sm:space-y-0">
+          {schoolProjects.map((project) => {
+            return (
+              <div key={project.projectname}>
+                <div className="border border-border rounded-md p-2 w-64">
+                  <p className="font-bold p-2 text-shadow">
+                    {project.projectname}
+                  </p>
+                  <p className="p-2 w-64">{project.discription}</p>
+                  <a href={project.link} className="p-2 hover:text-shadow">
+                    {project.linknavn}
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
